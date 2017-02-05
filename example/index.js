595f23d52364515995cd3e6607c292a8b8d233da
AvraamMavridis <avraammauridis@gmail.com>
2017-02-05 10:54:29 +0100

    fix linting of example

--- example/index.js
+++ example/index.js
@@ -5,55 +5,61 @@ import IntersectionVisible from '../src/index';
 class Example extends React.Component {
 
   state = {
-    visible: false,
-    active: true
+    visible : false,
+    active  : true,
   };
 
-  onIntersect = () => {
-    console.log('intersect');
+  onIntersect = () =>
+  {
+    console.log( 'intersect' );
   }
 
-  onShow = () => {
-    console.log('show');
-    this.setState({ visible : true })
+  onShow = () =>
+  {
+    console.log( 'show' );
+    this.setState( { visible : true } );
   };
 
-  onHide = () => {
-    console.log('hide');
-    this.setState({ visible : false})
+  onHide = () =>
+  {
+    console.log( 'hide' );
+    this.setState( { visible : false } );
   };
 
-  handleActiveCheckboxChange = (e) => {
-    this.setState({ active: e.target.checked })
+  handleActiveCheckboxChange = ( e ) =>
+  {
+    this.setState( { active : e.target.checked } );
   };
 
-  render() {
+  render()
+  {
     const { visible, active } = this.state;
-    const visibleState = visible ? "visible" : "invisible";
+    const visibleState = visible ? 'visible' : 'invisible';
     return (
       <div>
-        <div className={visibleState}>
-          {visibleState}
+        <div className={ visibleState }>
+          { visibleState }
           <label>
             <input type="checkbox"
-                   checked={active}
-                   onChange={this.handleActiveCheckboxChange}
+              checked={ active }
+              onChange={ this.handleActiveCheckboxChange }
             />
             Active
           </label>
         </div>
 
         <IntersectionVisible
-          onIntersect={this.onIntersect}
-          onShow={this.onShow}
-          onHide={this.onHide}
-          active={active}
-          className="element">
+          onIntersect={ this.onIntersect }
+          onShow={ this.onShow }
+          onHide={ this.onHide }
+          active={ active }
+          className="element"
+        >
           <div>{ visibleState }</div>
         </IntersectionVisible>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render( <Example />, document.getElementById( 'root' ) );
